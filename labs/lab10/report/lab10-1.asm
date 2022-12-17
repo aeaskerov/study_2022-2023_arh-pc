e659d0a3e5556240528bc932f4bfecdfca5b829f
Alexander Askerov <iqwertydragoni@gmail.com>
2022-12-17 15:58:49 +0300

    feat(main): add files lab-10

---
+++ labs/lab10/report/lab10-1.asm
@@ -0,0 +1,45 @@
+%include 'in_out.asm'
+
+SECTION .data
+    msg: DB 'Введите x: ',0
+    result: DB '2(3x-1)+7=',0
+
+SECTION .bss
+    x: RESB 80
+    rez: RESB 80
+
+SECTION .text
+GLOBAL _start
+_start:
+    mov eax,msg
+    call sprint
+
+    mov ecx,x
+    mov edx,80
+    call sread
+
+    mov eax,x
+    call atoi
+
+    call _calcul
+
+    mov eax,result
+    call sprint
+    mov eax,[rez]
+    call iprintLF
+
+    call quit
+
+_calcul:
+    mov ebx,2
+    call _subcalcul
+    mul ebx
+    add eax,7
+    mov [rez],eax
+    ret
+
+_subcalcul:
+    mov ecx,3
+    mul ecx
+    sub eax,1
+    ret
---
+++ labs/lab10/report/lab10-4.asm
@@ -0,0 +1,37 @@
+%include 'in_out.asm'
+SECTION .data
+    function db "Функция: f(x)=8x-3",0
+    result db "Результат: ",0
+
+SECTION .text
+    global _start
+_start:
+    mov eax,function
+    call sprintLF
+
+    pop ecx ;num. of arg-ts
+    pop edx ;prog. name
+    sub ecx,1 ;decrement ecx
+    mov esi,0 ;current sum
+
+next:
+    cmp ecx,0h ;if ecx is not empty
+    jz _end ;then: end prog.
+    pop eax ;else: take next ecx value from stack
+    call atoi ;int(eax)
+    call _calc
+    loop next
+
+_end:
+    mov eax,result
+    call sprint
+    mov eax,esi
+    call iprintLF
+    call quit
+
+_calc:
+    mov ebx,8
+    mul ebx
+    sub eax,3
+    add esi,eax
+    ret
